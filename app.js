4854c2de2d225a4bd8f5750bc83a583b70f1fdcd
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-11-28 21:38:11 -0700

    Initialized database setup with MongoDB, connected to the server using Mongoose (v6.6.1), and created schemas and models for User and ClothingItem resources. Added URL validation for avatar and imageUrl fields using the validator package.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express()
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
 const { PORT = 3001 } = process.env
 
 app.listen(PORT)
