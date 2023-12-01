5edd9055f614c5e4f6f005507a9bf155c4d59afa
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-01 15:48:43 -0700

    Implement error handling in controllers

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,19 +1,64 @@
-const ClothingItems = require('../models/clothingItem');
+const ClothingItems = require("../models/clothingItem");
+const { INVALID_DATA, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
 module.exports.getClothingItems = (req, res) => {
   ClothingItems.find({})
+    .then((items) => res.send({ data: items }))
+    .catch((err) => {
+      console.error(
+        `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
+      );
+      if (err.name === "DocumentNotFoundError") {
+        res
+          .status(NOT_FOUND)
+          .send({ message: "Requested clothing item not found" });
+      } else {
+        res
+          .status(SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+      }
+    });
 };
 
 module.exports.createClothingItem = (req, res) => {
-  // const { name, weatherType, imageUrl, _id } = req.body
-  console.log(req.user._id)
-  // ClothingItems.create({ name, weatherType, imageUrl, _id })
-  //   .then((item) => res.send({ data: item}))
-  //   .catch((err) => res.status(500).send({ message: 'Error'}))
+  const { name, weatherType, imageUrl } = req.body;
+  const userId = req.user._id;
+
+  ClothingItems.create({ name, weatherType, imageUrl, owner: userId })
+    .then((item) => res.send({ data: item }))
+    .catch((err) => {
+      console.error(
+        `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
+      );
+      if (err.name === "ValidationError") {
+        res
+          .status(INVALID_DATA)
+          .send({
+            message: "Invalid data provided for creating a clothing item",
+          });
+      } else {
+        res
+          .status(SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+      }
+    });
 };
 
 module.exports.deleteClothingItem = (req, res) => {
   ClothingItems.findByIdAndDelete(req.params.id)
-    .then((item) => res.send({ data: item}))
-    .catch((err) => res.status(500).send({ message: 'Error'}))
+    .then((item) => res.send({ data: item }))
+    .catch((err) => {
+      console.error(
+        `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
+      );
+      if (err.name === "DocumentNotFoundError") {
+        res
+          .status(NOT_FOUND)
+          .send({ message: "Requested clothing item not found" });
+      } else {
+        res
+          .status(SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+      }
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,40 @@
 const Users = require("../models/user");
+const { USER_NOT_FOUND, SERVER_ERROR, INVALID_DATA } = require('../utils/errors');
 
 module.exports.getUsers = (req, res) => {
-  Users.find(req.body)
+  Users.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: "Error" }));
+    .catch((err) => {
+      console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
+      res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
+    });
 };
 
 module.exports.getUser = (req, res) => {
-  Users.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "User  Not Found!" }));
+  Users.findById(req.params.userId)
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(USER_NOT_FOUND).send({ message: 'User not found' });
+      }
+    })
+    .catch((err) => {
+      console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
+      res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, avatar } = req.body;
-  Users.create({ name, avatar }).then((user) => res.send({ data: user }));
+  Users.create({ name, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
+      if (err.name === 'ValidationError') {
+        res.status(INVALID_DATA).send({ message: 'Invalid data provided for creating a user' });
+      } else {
+        res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
+      }
+    });
 };
