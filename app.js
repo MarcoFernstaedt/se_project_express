932436a5e46611d50bbae1517709c09fe8abd79e
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-11-30 21:54:22 -0700

    create autorization middleware for user _id temp fix for owner field

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '65695ff308ed995278dcee3c'
+  };
+  next();
+});
+
 app.use('/', routes);
 
 const { PORT = 3001 } = process.env;
--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -5,11 +5,11 @@ module.exports.getClothingItems = (req, res) => {
 };
 
 module.exports.createClothingItem = (req, res) => {
-  const { name, weatherType, imageUrl, userId } = req.body
-
-  ClothingItems.create({ name, weatherType, imageUrl, userId })
-    .then((item) => res.send({ data: item}))
-    .catch((err) => res.status(500).send({ message: 'Error'}))
+  // const { name, weatherType, imageUrl, _id } = req.body
+  console.log(req.user._id)
+  // ClothingItems.create({ name, weatherType, imageUrl, _id })
+  //   .then((item) => res.send({ data: item}))
+  //   .catch((err) => res.status(500).send({ message: 'Error'}))
 };
 
 module.exports.deleteClothingItem = (req, res) => {
