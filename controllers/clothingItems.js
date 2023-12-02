0eadb614e8ba1d8ce3e2cce8245ea3780ca7ed40
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-01 17:57:20 -0700

    Implement like/dislike routes, refactor using async/await

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,64 +1,58 @@
 const ClothingItems = require("../models/clothingItem");
 const { INVALID_DATA, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
-module.exports.getClothingItems = (req, res) => {
-  ClothingItems.find({})
-    .then((items) => res.send({ data: items }))
-    .catch((err) => {
-      console.error(
-        `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
-      );
-      if (err.name === "DocumentNotFoundError") {
-        res
-          .status(NOT_FOUND)
-          .send({ message: "Requested clothing item not found" });
-      } else {
-        res
-          .status(SERVER_ERROR)
-          .send({ message: "An error has occurred on the server." });
-      }
-    });
+module.exports.getClothingItems = async (req, res) => {
+  try {
+    const items = await ClothingItems.find({});
+    res.send({ data: items });
+  } catch (err) {
+    console.error(
+      `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
+    );
+
+    if (err.name === "DocumentNotFoundError") {
+      res.status(NOT_FOUND).send({ message: "Requested clothing item not found" });
+    } else {
+      res.status(SERVER_ERROR).send({ message: "An error has occurred on the server." });
+    }
+  }
 };
 
-module.exports.createClothingItem = (req, res) => {
-  const { name, weatherType, imageUrl } = req.body;
-  const userId = req.user._id;
+module.exports.createClothingItem = async (req, res) => {
+  try {
+    const { name, weatherType, imageUrl } = req.body;
+    const userId = req.user._id;
 
-  ClothingItems.create({ name, weatherType, imageUrl, owner: userId })
-    .then((item) => res.send({ data: item }))
-    .catch((err) => {
-      console.error(
-        `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
-      );
-      if (err.name === "ValidationError") {
-        res
-          .status(INVALID_DATA)
-          .send({
-            message: "Invalid data provided for creating a clothing item",
-          });
-      } else {
-        res
-          .status(SERVER_ERROR)
-          .send({ message: "An error has occurred on the server." });
-      }
-    });
+    const item = await ClothingItems.create({ name, weatherType, imageUrl, owner: userId });
+    res.send({ data: item });
+  } catch (err) {
+    console.error(
+      `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
+    );
+
+    if (err.name === "ValidationError") {
+      res.status(INVALID_DATA).send({
+        message: "Invalid data provided for creating a clothing item",
+      });
+    } else {
+      res.status(SERVER_ERROR).send({ message: "An error has occurred on the server." });
+    }
+  }
 };
 
-module.exports.deleteClothingItem = (req, res) => {
-  ClothingItems.findByIdAndDelete(req.params.id)
-    .then((item) => res.send({ data: item }))
-    .catch((err) => {
-      console.error(
-        `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
-      );
-      if (err.name === "DocumentNotFoundError") {
-        res
-          .status(NOT_FOUND)
-          .send({ message: "Requested clothing item not found" });
-      } else {
-        res
-          .status(SERVER_ERROR)
-          .send({ message: "An error has occurred on the server." });
-      }
-    });
+module.exports.deleteClothingItem = async (req, res) => {
+  try {
+    const item = await ClothingItems.findByIdAndDelete(req.params.id);
+    res.send({ data: item });
+  } catch (err) {
+    console.error(
+      `Error ${err.name} with the message ${err.message} has occurred while executing the code`,
+    );
+
+    if (err.name === "DocumentNotFoundError") {
+      res.status(NOT_FOUND).send({ message: "Requested clothing item not found" });
+    } else {
+      res.status(SERVER_ERROR).send({ message: "An error has occurred on the server." });
+    }
+  }
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,43 @@
 const Users = require("../models/user");
 const { USER_NOT_FOUND, SERVER_ERROR, INVALID_DATA } = require('../utils/errors');
 
-module.exports.getUsers = (req, res) => {
-  Users.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => {
-      console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
-      res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
-    });
+const getUsers = async (req, res) => {
+  try {
+    const users = await Users.find({});
+    res.send({ data: users });
+  } catch (err) {
+    console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
+    res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
+  }
 };
 
-module.exports.getUser = (req, res) => {
-  Users.findById(req.params.userId)
-    .then((user) => {
-      if (user) {
-        res.send({ data: user });
-      } else {
-        res.status(USER_NOT_FOUND).send({ message: 'User not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
-      res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
-    });
+const getUser = async (req, res) => {
+  try {
+    const user = await Users.findById(req.params.userId);
+    if (user) {
+      res.send({ data: user });
+    } else {
+      res.status(USER_NOT_FOUND).send({ message: 'User not found' });
+    }
+  } catch (err) {
+    console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
+    res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
+  }
 };
 
-module.exports.createUser = (req, res) => {
+const createUser = async (req, res) => {
   const { name, avatar } = req.body;
-  Users.create({ name, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
-      if (err.name === 'ValidationError') {
-        res.status(INVALID_DATA).send({ message: 'Invalid data provided for creating a user' });
-      } else {
-        res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
-      }
-    });
+  try {
+    const user = await Users.create({ name, avatar });
+    res.send({ data: user });
+  } catch (err) {
+    console.error(`Error ${err.name} with the message ${err.message} has occurred while executing the code`);
+    if (err.name === 'ValidationError') {
+      res.status(INVALID_DATA).send({ message: 'Invalid data provided for creating a user' });
+    } else {
+      res.status(SERVER_ERROR).send({ message: 'An error has occurred on the server.' });
+    }
+  }
 };
+
+module.exports = { getUsers, getUser, createUser };
