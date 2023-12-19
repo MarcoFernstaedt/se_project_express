6947f06da3106e2b84468dbb92d2fb5dd2ee30e1
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-18 17:55:35 -0700

    feat: search for existing user custome method

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,5 +37,23 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.statics.findUserByCredentials = findUserByCredentials (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        Promise.reject( new Error('Incorrect email or password'))
+      }
+
+      return bcrypt.compare(password, this.password)
+        .then((matched) => {
+          if (!matched) {
+
+          }
+
+          return user
+        })
+    })
+}
 
 module.exports = mongoose.model("user", userSchema);
