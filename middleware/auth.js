192d8986be1624df659a4e7774e31bb22bd258b8
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-19 11:13:02 -0700

    style: formatted doccument

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,24 @@
-const jwt = require('jsonwebtoken');
-const { UNAUTHORIZED, SERVER_ERROR } = require('../utils/errors');
-const { JWT_SECRET } = require('../utils/config')
+const jwt = require("jsonwebtoken");
+const { UNAUTHORIZED, SERVER_ERROR } = require("../utils/errors");
+const { JWT_SECRET } = require("../utils/config");
 
-module.exports.authorizationMiddleware =  (req, res, next) => {
-  const token = req.headers.authorization?.replace('Bearer ', '');
+module.exports.authorizationMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
   if (!token) {
-    return res.status(UNAUTHORIZED).json({ message: 'Unauthorized - Missing Token' });
+    return res
+      .status(UNAUTHORIZED)
+      .json({ message: "Unauthorized - Missing Token" });
   }
 
   try {
-    const payload = jwt.verify(token, JWT_SECRET)
+    const payload = jwt.verify(token, JWT_SECRET);
 
-    req.user = payload
+    req.user = payload;
 
-    next()
+    next();
   } catch (err) {
-    res.status(err.statusCode || SERVER_ERROR).send({ message: err.message || 'Internal sever error'})
+    res
+      .status(err.statusCode || SERVER_ERROR)
+      .send({ message: err.message || "Internal sever error" });
   }
-}
+};
