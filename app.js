52cb687d2f7a2968164872621209f45a0670f74c
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-19 17:28:52 -0700

    refactor: removed test auth added comments

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const helmet = require("helmet");
-
+// app setup
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
+// Connects to database
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+// allows server to recieve json
 app.use(express.json());
 
+// helmet security
 app.use(helmet());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "65695ff308ed995278dcee3c",
-  };
-  next();
-});
-
+// routes
 app.use("/", routes);
 
+// set port
 const { PORT = 3001 } = process.env;
 
+// set server to run on PORT
 app.listen(PORT);
