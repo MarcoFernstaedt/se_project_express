c2158a6213d197c6e0593072b64f014b2c9b884e
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-09-16 20:17:35 -0700

    wip: centralized error handling

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require('cors');
+const cors = require("cors");
 // app setup
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes");
+// Centralized Error Handling
+const errorHandlingMiddleWare = require("./middlewares/error");
 
 // Connects to database
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
@@ -15,13 +17,16 @@ app.use(express.json());
 // helmet security
 app.use(helmet());
 // cors to secure api request
-app.use(cors())
+app.use(cors());
 
 // routes
 app.use("/", routes);
 
 // set port
 const { PORT = 3001 } = process.env;
+
+// Centralized Error Handling
+app.use(errorHandlingMiddleWare);
 
 // set server to run on PORT
 app.listen(PORT);
