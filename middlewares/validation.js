59171d72e287a3662c29f3441da3158924bbcd40
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-11-01 12:58:25 -0700

    implament validation for user profile update

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -61,6 +61,20 @@ module.exports.validateLoginBody = celebrate({
   }),
 });
 
+module.exports.validateUpdateUserData = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+    }),
+
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'The "avatar" field must be a valid URL',
+    }),
+  }),
+});
+
 module.exports.validateId = celebrate({
   params: Joi.object().keys({
     itemId: Joi.string().length(24).hex().required().messages({
