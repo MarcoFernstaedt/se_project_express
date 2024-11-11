e27ad29dd447714a2f940e716ae9295154d277e1
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-11-11 16:09:24 -0700

    fix: eslint error

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const { UNAUTHORIZED } = require("../utils/errors");
 const UnauthorizedError = require("../errors/unauthorized");
 
 const userSchema = new mongoose.Schema({
